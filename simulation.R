ea4c0b8044391643700ec5c4c1efc73fe3128e2b
Ethan4444444 <ethan.pawl.22@gmail.com>
2025-04-17 00:35:23 -0700

    wrote Wisniowski posterior function

--- simulation.R
+++ simulation.R
@@ -86,3 +86,23 @@ all.samples <- list(prob.samples=prob.samples,
 
 save(all.samples,
      file="ACS_NPS_samples.RData")
+
+# TODO: test on small data w/ noninformative prior
+nps_post <- function(y, X, mu_beta, k0, V, a, b) {
+    n <- nrow(X)
+    p <- ncol(X)
+
+    XTX <- crossprod(X)
+    inv_scale_mat <- solve(XTX + solve(k0 * V)) # TODO: check why eqn. (9) is different
+    
+    beta_hat <- solve(XTX) %*% crossprod(X, y) # eqn 7
+    W <- inv_scale_mat %*% XTX # eqn 6
+    post_mn <- W %*% beta_hat + (diag(1, p) - W) %*% mu_beta # eqn 5
+
+    RSS <- crossprod(y - X %*% beta_hat) # eqn 10
+    bet_minus_mu <- beta_hat - mu_beta
+    SS <- RSS + crossprod(bet_minus_mu, solve(solve(XTX) + k0 * V) %*% bet_minus_mu) # eqn 9
+    post_var <- inv_scale_mat %*% (SS + 2 * b) / (n + 2 * a - 2) # eqn 8
+
+    return(list(mn = post_mn, vr = post_var))
+}
