007235b1183a5710c2e18058ad7a4eb6212e39bb
dvedensk <dvedensky@gmail.com>
2025-11-03 15:56:23 -0800

    Add flag for specifying which IPW method to return

--- code/utils.R
+++ code/utils.R
@@ -7,25 +7,53 @@ int_score <- function(alpha, truth, L, U){
 # Estimate pseudo‐inclusion weights for a non‐probability sample
 # via a logistic model distinguishing prob vs. non‐prob cases
 
-estimate_ipw <- function(ps, nps, cov_formula) {
-  # 1. Stack and label
-  combined <- bind_rows(
-    ps  %>% mutate(Z = 0),
-    nps %>% mutate(Z = 1)
-  )
+estimate_ipw <- function(ps, nps, cov_formula, method) {
+  if(! (method %in% c("ignorable", "beta_reg", "weighted"))) {
+    stop()
+  }
+
+  n_ps <- nrow(ps)
+  n_nps <- nrow(nps)
+  C_s <- n_ps/(n_nps + n_ps)
+  C_s_star <- n_nps/(n_nps + n_ps)
+  ps_weights <- ps$PWGTP #Need to double check whether scaling matters here
+      
+  if(method == "beta_reg") {
+    filter_id <- which(ps_weights == 1)
+    ps$PWGTP[filter_id] <- 1.001
+    beta_reg_out <- betareg::betareg(formula=as.formula(paste("1/PWGTP",
+                                                     deparse(X_formula))),
+                            data=ps)
+    nps_prob_pred <- predict(beta_reg_out, newdata=nps)    
+  } else if(method == "weighted") {
+    combined <- bind_rows(
+      ps  %>% mutate(Z = 0),
+      nps %>% mutate(Z = 1)
+    )
+
+    combined <- combined %>%
+        mutate(PWGTP = ifelse(Z == 1, 1, PWGTP))         
+        
+    fit_prop <- glm(
+      formula = as.formula(paste0("Z ", deparse(cov_formula))),
+      weights = PWGTP, 
+      family  = binomial(),
+      data    = combined
+    )
+
+    nps_prob_pred  <- predict(fit_prop, newdata = nps, type = "response")
+  }
+  nps_weights <- 1/nps_prob_pred
   
-  # 2. Fit propensity‐to‐be‐nonprob logistic regression
-  fit_prop <- glm(
-    formula = as.formula(paste0("Z ", deparse(cov_formula))),
-    family  = binomial(),
-    data    = combined
-  )
-  
-  # 3. Predict P(Z=1 | X) on nonprob units
-  p_hat  <- predict(fit_prop, newdata = nps, type = "response")
-  ipw_raw <- 1 / p_hat
-  
-  # 4. Normalize so sum(ipw) = nrow(nps) (keeps scale comparable to PS size)
-  ipw <- ipw_raw * (nrow(nps) / sum(ipw_raw))
-  return(ipw)
+  # Normalize 
+  nps_weights <- nps_weights * (C_s_star * sum(nps_weights)/sum(ps_weights))
+  ps_weights <- ps_weights * C_s
+
+  return(list(nps_ipw = nps_ipw,
+              ps_ipw = ps_ipw)
 }
+
+
+    
+
+#brm_out <- brm(1/PWGTP ~ AGEP + RAC1P + SEX, data=ps, family=Beta())
