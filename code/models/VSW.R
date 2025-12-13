016475c3d45ae3a9ecd95ea505a2585cb607464c
Qi Wang <1061673783@qq.com>
2025-12-12 16:31:59 -0800

    Update VSW.R

--- code/models/VSW.R
+++ code/models/VSW.R
@@ -1,12 +1,26 @@
-vsw_out <- function(ps, nps, X_formula){
+
+vsw_out <- function(ps, nps, X_formula, response){
   # 3. Build design matrices for PS
   X_ps   <- model.matrix(X_formula,   data = ps)
-  y_ps   <- ps$HICOV
   X_nps <- model.matrix(X_formula, data = nps)
-  y_nps <- nps$HICOV
+  y_ps  <- ps[[response]]
+  y_nps <- nps[[response]]
+  
+  all_dat <- rbind(ps, nps)
+  X_all <- model.matrix(X_formula, data = all_dat)
+  y_all <- all_dat[[response]]
+  
+  
   #fit models here:
   model_ps <- glm(y_ps~X_ps, family = binomial(link = "logit"))
   model_nps <- glm(y_nps~X_nps, family = binomial(link = "logit"), data = nps)
+  model_all <- glm(y_all~X_all, family = binomial(link = "logit"), data = all_dat)
+  
+  z_hat_pool <- data.frame(
+    "PUMA" = all_dat$PUMA, "prediction" = predict(model_all, type = "response")
+  ) %>% group_by(PUMA) %>% summarise(Z_k1 = mean(prediction), Z_k0 = 1-mean(prediction))
+  
+  
   Z_hat_ps <- data.frame(
     "PUMA" = ps$PUMA, "prediction" = predict(model_ps, type = "response")
   ) %>% group_by(PUMA) %>% summarise(Z_k1 = mean(prediction), Z_k0 = 1-mean(prediction)) 
@@ -22,14 +36,14 @@ vsw_out <- function(ps, nps, X_formula){
     count(PUMA, name = "n_nps")%>%
     semi_join(ps, by = "PUMA")
   
-  ps_count <- ps %>% count(PUMA, name = "n_nps")
+  ps_count <- ps %>% count(PUMA, name = "n_ps")
   
   K <- nrow(Z_hat_ps)
-  C <- length(unique(ps$HICOV))
+  C <- length(unique(ps[[response]]))
   # if(nrow(Z_hat_ps) != nrow(Z_hat_nps)){stop("Probability sample and non-probability sample domains dismatch.")}
   hat_beta_c <- colMeans(Z_combined[,4:5] - Z_hat_ps[,2:3])
-  hat_sig <- sum((Z_combined[,4:5] - Z_combined[,2:3]) - 
-                   matrix(hat_beta_c, nrow = nrow(Z_combined), ncol = 2, byrow = TRUE)^2)/(C*(K-1))
+  hat_sig <- sum((Z_combined[,4:5] - Z_combined[,2:3] - 
+                    matrix(hat_beta_c, nrow = nrow(Z_combined), ncol = 2, byrow = TRUE))^2)/(C*(K-1))
   hat_nu_kc <- Z_combined[,4:5]*(1-Z_combined[,4:5])
   combined_results <- matrix(NA, nrow = K, ncol = C)
   for (k in 1:K) {
@@ -45,11 +59,5 @@ vsw_out <- function(ps, nps, X_formula){
   }
   
   return(
-    data.frame("PUMA" = Z_hat_nps$PUMA, "point_est" = combined_results[,1], "lower_CI" = NA, "upper_CI" = NA, "model" = "VSW")
-)
-    }
-  
-
-
-
-
+    data.frame("PUMA" = Z_hat_nps$PUMA, "VSW_point_est" = combined_results[,1], "ps_est" = Z_hat_ps[,2,drop = TRUE], "nps_est" = Z_hat_nps[,2,drop = TRUE], "pooled_results" = z_hat_pool[,2, drop = TRUE], "lower_CI" = NA, "upper_CI" = NA, "model" = "VSW") )
+}
